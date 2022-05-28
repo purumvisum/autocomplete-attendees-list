71d37e3773e0fe670f11cd9c663b585261c9a680
purumvisum <purumvisum@gmail.com>
2022-05-28 20:14:40 +0200

    clean tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,26 +8,3 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-
-// when press @ at the end
-// when press @ at the start
-// when press @ in the middle
-
-// when start editing red span
-// when press delete in red span
-// when press "a" in red span
-
-// when delete all info from text
-// если начал редактировать уже контакт и нажал @
-
-// если начал вводить @ но ничего не выбрал  ?
-// Если несколько одинаковых контактов в списке
-
-// Курсор почему-то скачет ? если менять значение контакта
-// записать все в стор Участников
-
-// Управлять кнопками вниз
-
-
-// Issues:
-// If I removed Name manualy
